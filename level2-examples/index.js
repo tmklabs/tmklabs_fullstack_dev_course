62ddfb9a160b45796f5cfc7c519a159577e76ccf
aarivtmk <aariv.tmk@gmail.com>
2023-12-18 18:03:00 +0530

    explained http servers - url,route,query params  #Please enter the commit message for your changes. Lines starting

---
+++ level2-examples/index.js
@@ -0,0 +1,78 @@
+// within the machine access
+/* let sum = 0;
+const addition = (number) => {
+  for (let i = 1; i < number; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+
+let result = addition(100);
+console.log("answer is", result);
+ */
+
+// give access to others
+/* 
+install express
+setup a express http server
+*/
+
+/* const express = require("express");
+const app = express();
+const port = 3000;
+
+let sum = 0;
+const addition = (number) => {
+  for (let i = 1; i < number; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+
+// Defining a route on the app
+app.get("/", (req, res) => {
+  let result = "addition is " + addition(100);
+  res.send(result);
+});
+
+// Starting the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+ */
+
+// query paramas
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let sum = 0;
+const addition = (number) => {
+  for (let i = 1; i < number; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+
+// Defining a route on the app
+app.get("/", (req, res) => {
+  let result = "addition is " + addition(req.query.lastNumber);
+  res.send(result);
+});
+
+app.get("/multipleparams", (req, res) => {
+  let result =
+    "addition is " +
+    addition(req.query.lastNumber) +
+    "seond param is" +
+    req.query.a +
+    "third param is" +
+    req.query.b;
+  res.send(result);
+});
+
+// Starting the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
